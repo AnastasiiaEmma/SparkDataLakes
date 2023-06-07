9c6e37d5a8e3dfc878638958233ce64cfd64a51e
Anastasiia Shvaiko <anastasiia.shvaiko@monstar-lab.com>
2023-06-07 13:22:30 +0200

    Trusted script and sch updates

--- trusted/customer/accelerometer/customer_accelerometer_landing_to_trusted.py
+++ trusted/customer/accelerometer/customer_accelerometer_landing_to_trusted.py
@@ -18,13 +18,18 @@ customer_landing = spark.read.json("s3://balalal/customer_landing/")
 accelerometer_landing = spark.read.json("s3://balalal/accelerometer_landing/")
 
 # Perform data sanitization and filtering operations
-sanitized_customer = customer_landing.filter(customer_landing['sharewithresearchasofdate'].isNull())
+sanitized_customer = customer_landing.filter(customer_landing['shareWithResearchAsOfDate'].isNotNull())
 
 # Select the desired columns from the sanitized customer data
-customer_trusted = sanitized_customer.select('customerName', 'email', 'phone', 'birthDay', 'serialNumber', 'registrationDate', 'lastUpdateDate', 'shareWithResearchAsOfDate', 'shareWithPublicAsOfDate', 'shareWithFriendsAsOfDate')
+customer_trusted = sanitized_customer.select(
+    'customerName', 'email', 'phone', 'birthDay', 'serialNumber', 'registrationDate',
+    'lastUpdateDate', 'shareWithResearchAsOfDate', 'shareWithPublicAsOfDate', 'shareWithFriendsAsOfDate'
+)
 
 # Filter the accelerometer data for customers who have agreed to share
-sanitized_accelerometer = accelerometer_landing.join(sanitized_customer, accelerometer_landing['user'] == sanitized_customer['email'])
+sanitized_accelerometer = accelerometer_landing.join(
+    sanitized_customer, accelerometer_landing['user'] == sanitized_customer['email']
+)
 
 # Select the desired columns from the sanitized accelerometer data
 accelerometer_trusted = sanitized_accelerometer.select('timeStamp', 'user', 'x', 'y', 'z')
